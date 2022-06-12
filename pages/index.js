4d04e85f5abba5e78fb9d6696f363fab0604b4c6
kamoto <takahiro.08039@gmail.com>
2022-06-12 14:32:23 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -50,6 +51,9 @@ export default function Home() {
             </p>
           </a>
         </div>
+        <Link href="/posts">
+          <a>POST一覧へ遷移する</a>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
